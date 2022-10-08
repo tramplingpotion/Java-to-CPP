3422b2599dc1f4b4a2647e4d4764ecf4ae57d9f6
tramplingpotion <syamilfahman7007@gmail.com>
2022-10-08 10:44:12 +0800

    added loop to gambling game
    the game has loop that asks user whether to continue the game or not

--- Simple/Gambling.cpp
+++ Simple/Gambling.cpp
@@ -9,21 +9,33 @@ int main(int argc, char const *argv[])
     int number;
     int winningNum;
 
+    char cont = 'y';
+
     srand(time(0));
 
-    std::cout<<"Welcome to CPP Gambling!"<<std::endl;
-    std::cout<<"Choose between 1 to 10: "<<std::endl;
-    std::cin>>number;
+    while (cont == 'y')
+    {
+        std::cout << "Welcome to CPP Gambling!" << std::endl;
+        std::cout << "Choose between 1 to 10: " << std::endl;
+        std::cin >> number;
 
-    winningNum = rand()%(maxVal-minVal+1)+minVal;
+        winningNum = rand() % (maxVal - minVal + 1) + minVal;
 
-    if(winningNum==number){
-        std::cout<<"You Win!!!"<<std::endl;
-    }else{
-        std::cout<<"You Lose..."<<std::endl;
-        std::cout<<"The Winning Number is "<<winningNum<<std::endl;
+        if (winningNum == number)
+        {
+            std::cout << "You Win!!!" << std::endl;
+        }
+        else
+        {
+            std::cout << "You Lose..." << std::endl;
+            std::cout << "The Winning Number is " << winningNum << std::endl;
+        }
 
+        std::cout << "Do you want to continue? (y/n)" << std::endl;
+        std::cin >> cont;
     }
+
+    std::cout << "Gambling Game Terminated...";
 
     return 0;
 }
